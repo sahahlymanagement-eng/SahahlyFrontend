edbff8e959541cdfa833a64d3d473d4cf56b13f0
yaraeslamm <yaraelshehawi@gmail.com>
2026-05-11 22:41:15 +0300

    edit double imports in coursework.jsx

--- src/pages/manager/CourseManagment/CourseWork.jsx
+++ src/pages/manager/CourseManagment/CourseWork.jsx
@@ -1,6 +1,5 @@
 
 import { useState, useEffect } from "react";
-import { useState } from "react";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import api from "../../../api/api";
 import { toast } from "react-toastify";
