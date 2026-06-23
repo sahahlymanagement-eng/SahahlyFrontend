52ead5c2ee411fba5a37a876b2b0e6be8e55f6e2
youssefrizk2 <166840867+youssefrizk2@users.noreply.github.com>
2026-06-23 14:54:13 +0300

    fix(landing page): Fix responsiveness on mobile and tablet

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/images/Logo.png";
+import logo from "../assets/images/Logo-removebg-preview.png";
 import "./LandingPage.css";
 
 const AppIcon = () => (
